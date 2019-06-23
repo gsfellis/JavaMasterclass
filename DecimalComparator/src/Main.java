819395698da403d7e1576cb4ecdc8f6306bce23a
gsfellis <gregfellis@gmail.com>
2019-06-23 10:18:46 -0400

    Reorganize by section and add new exercises

--- DecimalComparator/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(1.214, 1.2145));
-
-    }
-}
--- EqualSumChecker/src/EqualSumChecker.java
+++
@@ -1,6 +0,0 @@
-public class EqualSumChecker {
-
-    public static boolean hasEqualSum(int x, int y, int z) {
-        return x + y == z;
-    }
-}
